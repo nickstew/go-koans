98591c59077fde80d9b4a2951e6becfa38caa8a2
Nick Stewart <nick.stewart@rakuten.com>
2015-04-13 11:44:31 -0600

    Meditation on common interfaces

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -9,14 +9,7 @@ func aboutCommonInterfaces() {
 
 		out := new(bytes.Buffer)
 
-		/*
-		   Your code goes here.
-		   Hint, use these resources:
-
-		   $ godoc -http=:8080
-		   $ open http://localhost:8080/pkg/io/
-		   $ open http://localhost:8080/pkg/bytes/
-		*/
+		in.WriteTo(out)
 
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
@@ -26,6 +19,9 @@ func aboutCommonInterfaces() {
 		in.WriteString("hello world")
 
 		out := new(bytes.Buffer)
+
+		in.WriteTo(out)
+		out.Truncate(5) // unsure if this is what they wanted me to use or use io.CopyN
 
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
